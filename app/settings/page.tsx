b43900fbdf2e1dfd81cd7c04b0d6fdb5fbec6cca
esperanzaricardo-code <esperanza.ricardo@gmail.com>
2026-06-17 19:25:22 +0100

    Update page.tsx

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,16 +2,16 @@ import { ThemeProvider } from '@/components/ThemeProvider'
 import AnimatedBackground from '@/components/AnimatedBackground'
 import Nav from '@/components/Nav'
 import SettingsLayout from '@/components/SettingsLayout'
-
+import Footer from '@/components/Footer'
 export const revalidate = 0
 export const dynamic = 'force-dynamic'
-
 export default function SettingsPage() {
   return (
     <ThemeProvider>
       <AnimatedBackground />
       <Nav />
       <SettingsLayout />
+      <Footer />
     </ThemeProvider>
   )
 }
